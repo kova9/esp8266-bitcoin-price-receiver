769abe3598df41dea945b0988c1c5778ed9e4d21
KovacevicTom <kovacevictom@sbsit.com>
2024-04-16 13:49:11 +0200

    config

---
+++ config-template.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi credentials
+char ssid[] = "YOUR WIFI";
+char password[] = "YOUR PW";
+
+char key[] = "YOUR KEY";
+char url[] = "YOUR URL";
+
+char localServer[] = "YOUR ENDPOINT"; 
+char endpointLatest[] = "YOUR ENDPOINT";
+char endpointPOST[] = "YOUR ENDPOINT";
+
+#endif
